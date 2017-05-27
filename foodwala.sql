fbc01edc818857442a781ee79ee15f7ccbdde86e
v786 <jahangirali786@gmail.com>
2017-05-27 10:49:20 +0530

    SQL Dump

--- foodwala.sql
+++ foodwala.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 27, 2017 at 06:53 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.20
+-- Generation Time: May 27, 2017 at 07:17 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -30,6 +30,97 @@ CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `pass` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `item` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `oTime` date NOT NULL,
+  `stat` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cartItem`
+--
+
+CREATE TABLE `cartItem` (
+  `id` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `item` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coupon`
+--
+
+CREATE TABLE `coupon` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `type` int(2) NOT NULL,
+  `val` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dineIn`
+--
+
+CREATE TABLE `dineIn` (
+  `id` int(11) NOT NULL,
+  `restId` int(11) NOT NULL,
+  `bTime` date NOT NULL,
+  `bDate` date NOT NULL,
+  `pNo` int(11) NOT NULL,
+  `uid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL,
+  `rest_id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `category` text NOT NULL,
+  `sub` text NOT NULL,
+  `price` int(11) NOT NULL,
+  `logo` text NOT NULL,
+  `desk` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurant`
+--
+
+CREATE TABLE `restaurant` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `location` text NOT NULL,
+  `logo` text NOT NULL,
+  `phone` varchar(200) NOT NULL,
+  `oTime` date NOT NULL,
+  `category` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
