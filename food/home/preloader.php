f32b263f9bd8647104906364b59adc6be04b6575
v786 <jahangirali786@gmail.com>
2017-05-28 19:00:35 +0530

    Latest

---
+++ food/home/preloader.php
@@ -0,0 +1,9 @@
+	<div id="preloader">
+        <div class="sk-spinner sk-spinner-wave" id="status">
+            <div class="sk-rect1"></div>
+            <div class="sk-rect2"></div>
+            <div class="sk-rect3"></div>
+            <div class="sk-rect4"></div>
+            <div class="sk-rect5"></div>
+        </div>
+    </div>
